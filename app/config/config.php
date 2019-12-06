62afc3606acbb218d4b4d253f29416e2c824a2d4
Benjamin Marquis <bmarquiscom@gmail.com>
2019-12-06 16:53:50 -0500

    git init

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+  //App root
+  define("APPROOT", dirname(dirname(__FILE__)));
+
+  // URL Root
+  define("URLROOT", "http://localhost");
+
+  // Site Name
+  define("SITENAME", "Your App Name");
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Get needed files for the app to run
+require_once './app/bootstrap.php';
+
+$init = new Core();
